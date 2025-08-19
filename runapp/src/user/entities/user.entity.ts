828c0c5f170b6d023baf83e4fe2258ab6f5ee193
Maury SO <lynuxazura@gmail.com>
2025-08-18 19:32:26 -0600

    Integrity PostGress

---
+++ runapp/src/user/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({
+    type: "text",
+    unique: true,
+    nullable: false
+  })
+  email: string
+
+}
